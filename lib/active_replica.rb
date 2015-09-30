f58188af3c17f409c6bdd5e7a2156dad6a5247fd
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2015-09-30 12:23:24 +0800

    Logging should not be enabled by default
    I think this might affect performance

--- lib/active_replica.rb
+++ lib/active_replica.rb
@@ -1,6 +1,5 @@
 require 'active_replica/version'
 require 'active_replica/connection_handler'
-require 'active_replica/logging'
 require 'active_replica/railtie' if defined?(Rails)
 
 module ActiveReplica
